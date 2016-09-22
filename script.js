3808fec51f885ac6440e5dc89221bd31ad594727
Amanda Oh <[DAmandaGoh>
2016-09-23 01:03:23 +0800

    Answers up till Qn 8

--- script.js
+++ script.js
@@ -32,3 +32,87 @@ function wordReverse (words){
 }
 
 wordReverse(sentence);
+
+//Short Questions
+
+//Qn 1
+
+function maxOfTwoNumbers (num1 , num2) {
+  if (num1 > num2){
+    console.log(num1);
+  }
+  else {
+    console.log(num2)
+  }
+}
+
+maxOfTwoNumbers (3,10);
+
+//Qn 2
+function maxOfThree (num1, num2, num3){
+  if (num1 > (num2 && num3)){
+    console.log(num1);
+  } else if (num2 > (num1 && num3)){
+    console.log(num2);
+  } else {
+    console.log(num3);
+  }
+}
+
+maxOfThree(1,10,7);
+
+//Qn 3
+function isCharacterAVowel(string){
+  var x = string.toLowerCase();
+  if (x == "a" || x == "e" || x == "i" || x == "o" || x == "u" ){
+    console.log(true);
+  }
+  else {
+    console.log(false);
+  }
+}
+
+isCharacterAVowel("E");
+
+//Qn 4
+function sumArray (array){
+  for(var i=0, sum= 0; i < array.length; i++)
+    sum += array[i];
+     console.log(sum);
+}
+
+sumArray([1,2,3,4]);
+
+function multiplyArray (array){
+  for(var i=0, sum=1; i < array.length; i++)
+    sum *= array[i];
+    console.log(sum);
+}
+
+multiplyArray([1,2,3,4]);
+
+//Qn 5
+function NoofArg(a,b,c,d,e,f){
+  console.log(arguments.length);
+}
+
+//Qn 6
+NoofArg(1,2,3,4,5,6);
+
+function reverseString(str){
+  var splitString = str.split("");
+  var reverseArray = splitString.reverse();
+  var joinArray = reverseArray.join("");
+  console.log(joinArray);
+}
+
+reverseString("My Name is Amanda.");
+
+//Qn 7
+function findLongestWord (array){
+  var splitArray = array.split(" ");
+  var sortwords = splitArray.sort(function (a,b) {return b.length - a.length});
+  console.log(sortwords[0]);
+}
+
+findLongestWord("Hello javascript is quite difficult");
