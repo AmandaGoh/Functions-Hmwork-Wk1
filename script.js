0c2b087c5d5a2828b201dd65570cdfbe008717f8
Amanda Oh <[DAmandaGoh>
2016-09-23 15:51:52 +0800

    Final answers for Qn 1 to Qn 7

--- script.js
+++ script.js
@@ -5,21 +5,24 @@
 var words = ['hello', 'what', 'is', 'up', 'dude']
 
  function lengths(words) {
-   for (var i=0; i < words.length; i++);
-   console.log(words[i].length);
-}
+   var wordsArr = [];
+   for (var i=0; i < words.length; i++) {
+   wordsArr.push(words[i].length);
+   }
+   return wordsArr;
+ }
 
-lengths(words);
+console.log(lengths(words));
 
 //Qn 2
 function transmogrifier (num1, num2, num3){
   var product = num1 * num2;
-  console.log (Math.pow(product, num3));
+  return Math.pow(product, num3)
 }
 
-transmogrifier(5, 4, 3); //Answer: 8000
-transmogrifier(13, 12, 5); //Answer: 92389579776
-transmogrifier(42, 13, 7); //Answer: 14466001271480793000
+console.log(transmogrifier(5, 4, 3)); //Answer: 8000
+console.log(transmogrifier(13, 12, 5)); //Answer: 92389579776
+console.log(transmogrifier(42, 13, 7)); //Answer: 14466001271480793000
 
 //Qn 3
 var sentence="Let's see if we can write this in reverse."
@@ -30,92 +33,98 @@ function wordReverse (words){
   for (var i= wordssplit.length - 1; i >= 0; i--)
     x = x + wordssplit[i] + " ";
     if (i= wordssplit.length - 1){
-      console.log (x);
+     return x
     }
 }
 
-wordReverse(sentence);
+console.log(wordReverse(sentence));
 
 //Short Questions
 
 //Qn 1
 
 function maxOfTwoNumbers (num1 , num2) {
+  var longerNum = num1
   if (num1 > num2){
-    console.log(num1);
+    longerNum = num1
   }
   else {
-    console.log(num2)
+    longerNum = num2
   }
+  return longerNum
 }
 
-maxOfTwoNumbers (3,10);
+console.log(maxOfTwoNumbers(8,10));
 
 //Qn 2
 function maxOfThree (num1, num2, num3){
+  var largestnum =num1
   if (num1 > (num2 && num3)){
-    console.log(num1);
+    largestnum = num1;
   } else if (num2 > (num1 && num3)){
-    console.log(num2);
+    largestnum = num2;
   } else {
-    console.log(num3);
+    largestnum = num3;
   }
+  return largestnum
 }
 
-maxOfThree(1,10,7);
+console.log(maxOfThree(1,10,7));
 
 //Qn 3
 function isCharacterAVowel(string){
   var x = string.toLowerCase();
+  var isaVowel = false;
   if (x == "a" || x == "e" || x == "i" || x == "o" || x == "u" ){
-    console.log(true);
+    isaVowel = true;
   }
   else {
-    console.log(false);
+    isaVowel = false;
   }
+  return isaVowel
 }
 
-isCharacterAVowel("E");
+console.log(isCharacterAVowel("a"));
 
 //Qn 4
 function sumArray (array){
   for(var i=0, sum= 0; i < array.length; i++)
     sum += array[i];
-     console.log(sum);
+    return sum
 }
 
-sumArray([1,2,3,4]);
+console.log(sumArray([1,2,3,4]));
 
 function multiplyArray (array){
   for(var i=0, sum=1; i < array.length; i++)
     sum *= array[i];
-    console.log(sum);
+    return sum
 }
 
-multiplyArray([1,2,3,4]);
+console.log(multiplyArray([1,2,3,4]));
 
 //Qn 5
 function NoofArg(a,b,c,d,e,f){
-  console.log(arguments.length);
+  return arguments.length
 }
 
-//Qn 6
-NoofArg(1,2,3,4,5,6);
+console.log(NoofArg(1,2,3,4,5,6));
 
+//Qn 6
 function reverseString(str){
   var splitString = str.split("");
   var reverseArray = splitString.reverse();
   var joinArray = reverseArray.join("");
-  console.log(joinArray);
+  return joinArray;
 }
 
-reverseString("My Name is Amanda.");
+console.log(reverseString("My Name is Amanda."));
 
 //Qn 7
 function findLongestWord (array){
   var splitArray = array.split(" ");
   var sortwords = splitArray.sort(function (a,b) {return b.length - a.length});
-  console.log(sortwords[0]);
+  return sortwords[0];
 }
 
-findLongestWord("Hello javascript is quite difficult");
+console.log(findLongestWord("Hello javascript is quite difficult"));
